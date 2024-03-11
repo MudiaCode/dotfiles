bfbb7a3e08c1238919956e45321720105938f14d
Mudia Osagie <mudia.personal@protonmail.com>
2024-03-11 17:23:14 +0000

    add fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    alias hyprlandc="nvim .config/hypr/hyprland.conf"
+    alias hyprlock="nvim .config/hypr/hyprlock.conf"
+    alias fishc="nvim .config/fish/config.fish"
+    alias kittyc="nvim .config/kitty/kitty.conf"
+    
+    alias v="nvim"
+    alias p="sudo pacman -Sy"
+    alias pr="sudo pacman -R"
+    alias y="yay -S"
+    alias yr="yay -R"
+end
+
+set fish_greeting ""
